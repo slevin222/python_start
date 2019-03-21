f2348aebea5679daab7a4105de8bb198d9802ed1
Shawn Levin <shawnlevin22@gmail.com>
2019-03-20 23:49:46 -0700

    students

--- students.py
+++ students.py
@@ -3,9 +3,11 @@ students = {
             "Bob": {"id": "ID0002", "age":27, "grade":"B"},
             "Claire": {"id": "ID0003", "age":17, "grade":"C"},
             "Dan": {"id": "ID0004", "age":21, "grade":"D"},
-            "Emma":{"id": "ID0005", "age":22, "grade":"E"}
+            "Emma":{"id": "ID0005", "age":22, "grade":"E"},
+            "Shawn": {"id": "ID006", "age" : 24, "grade":"A"}
             }
 
 print(students["Dan"]["age"])
+print(students["Shawn"]["age"])
 
 print(students["Emma"]["id"], students["Emma"]["grade"])
